588e9d987249bbc65e888509f05e21eae36ee6d8
Salman Vajid <s@vajid.com>
2013-09-24 19:29:03 -0400

    Finished Array Exercise

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,7 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
-
-
+var myFavCars = ['Nissan', 'Honda', 'chevy', 'Beamer', 'Benz', 'Bentley'];
+for (i = 0; i <= myFavCars.length-1; i++){
+	console.log("My #" + (i+1) + "choice is " + myFavCars[i]);
+}
